82fb97c74e895b87c73b564b93660c13638c1371
ivanfright <71205975+ivanfright@users.noreply.github.com>
2023-11-19 22:40:09 +0800

    fixing

--- Assets/Scripts/HappinessCheck.cs
+++ Assets/Scripts/HappinessCheck.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HappinessCheck: MonoBehaviour
+public class HappinessCheck
 {
     public void HappyCheck()
     {
