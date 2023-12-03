2c92089b06dcdf7a12acb14aafc0df73b718c92a
ivanfright <71205975+ivanfright@users.noreply.github.com>
2023-12-03 14:51:18 +0800

    Movement but buggy

--- Assets/Scripts/Cats/CatSpawner.cs
+++ Assets/Scripts/Cats/CatSpawner.cs
@@ -7,55 +7,57 @@ public class CatSpawner : MonoBehaviour
 
     public GameObject[] cats;
 
-    [SerializeField]
-    private float initialSpawnTime;
-    [SerializeField]
-    private int spawnLimit;
+    [SerializeField] private float initialSpawnTime;
+    [SerializeField] private int spawnLimit;
+    [SerializeField] private float speed;
 
     private float spawnTime;
+    private Vector3 moveDirection;
 
-    public float speed;
-    public Vector3 moveDirection = new Vector3(1f, 1f, 0f);
+    private static readonly Vector3 DefaultMoveDirection = new Vector3(1f, 1f, 0f);
 
-    // Start is called before the first frame update
     void Start()
     {
         spawnTime = initialSpawnTime;
         SetRandomMoveDirection();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Check if there are more cats to spawn
         if (spawnLimit > 0)
         {
             spawnTime -= Time.deltaTime;
 
             if (spawnTime <= 0)
             {
-                // Instantiate a random cat from the array
-                Instantiate(cats[Random.Range(0, cats.Length)], transform.position, Quaternion.identity);
-
-                // Reset spawn time and decrement spawn limit
+                SpawnCat();
                 spawnTime = initialSpawnTime;
-                spawnLimit -= 1;
-
-                // Change the move direction after spawning a cat
+                spawnLimit--;
                 SetRandomMoveDirection();
             }
             else
             {
-                // Move the spawner
-                Vector3 movement = moveDirection.normalized * speed * Time.deltaTime;
-                transform.Translate(movement);
+                MoveSpawner();
             }
         }
     }
 
-    // Set a random move direction
     private void SetRandomMoveDirection()
     {
-        moveDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f).normalized;
+        moveDirection = Random.onUnitSphere; // Random vector on the surface of a sphere
+        moveDirection.z = 0f; // Keep it in the XY plane
+    }
+
+    private void SpawnCat()
+    {
+        Vector3 spawnPosition = transform.position; // You might want to adjust this for more control
+        Instantiate(cats[Random.Range(0, cats.Length)], spawnPosition, Quaternion.identity);
+    }
+
+    private void MoveSpawner()
+    {
+        Vector3 movement = moveDirection.normalized * speed * Time.deltaTime;
+        transform.Translate(movement);
     }
 }
+
