546d4d3f6eeacc18f7414bbf039e0f20517cbc30
GepardLandau4 <2201733I@student.tp.edu.sg>
2024-01-08 15:35:52 +0800

    walking and idle player animation

--- Assets/Scripts(MainWorld)/JoystickMovement.cs
+++ Assets/Scripts(MainWorld)/JoystickMovement.cs
@@ -7,10 +7,13 @@ public class JoystickMovement : MonoBehaviour
     public float speed;
     public Rigidbody2D rb;
     public Joystick joystick;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
     void Start()
     {
-
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -24,5 +27,22 @@ public class JoystickMovement : MonoBehaviour
         float moveV = joystick.Vertical;
         Vector2 moveDir = new Vector2 (moveH, moveV);
         rb.velocity = moveDir * speed;
+
+        if (moveH != 0 || moveV != 0)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+        if (moveH < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
     }
 }
